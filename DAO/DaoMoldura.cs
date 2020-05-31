6fc79ad1ca1dfec198a56c07ebff774402227407
alexandra <alexandraf0925@gmail.com>
2020-05-31 14:24:19 -0500

    DAO

---
+++ DAO/DaoMoldura.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using DTO;
+
+namespace DAO
+{
+    public class DaoMoldura
+    {
+        SqlConnection conexion;
+            public DataTable ListarMolduras()
+        {
+            DataTable dtmolduras = null;
+            conexion.Open();
+            SqlCommand command = new SqlCommand("SP_Listar_Molduras", conexion);
+            SqlDataAdapter daAdaptador = new SqlDataAdapter(command);
+            command.CommandType = CommandType.StoredProcedure;
+            dtmolduras = new DataTable();
+            daAdaptador.Fill(dtmolduras);
+            conexion.Close();
+            return dtmolduras;
+        }
+            public void RegistrarMoldura(DtoMoldura objmoldura)
+        {
+            SqlCommand command = new SqlCommand("SP_Registar_Moldura", conexion);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@descripcion", objmoldura.VM_Descripcion);
+            command.Parameters.AddWithValue("@imagen", objmoldura.VBM_Imagen);
+            command.Parameters.AddWithValue("@unidadMet", objmoldura.DM_UnidadMetrica);
+            command.Parameters.AddWithValue("@stock", objmoldura.IM_Stock);
+            command.Parameters.AddWithValue("@precio", objmoldura.DM_Precio);
+            command.Parameters.AddWithValue("@estado", objmoldura.IM_Estado);
+            command.Parameters.AddWithValue("@idtipom", objmoldura.FK_ITM_Tipo);
+            conexion.Open();
+            command.ExecuteNonQuery();
+            conexion.Close();
+        }
+            public void ActualizarMoldura(DtoMoldura objmoldura)
+        {
+            SqlCommand command = new SqlCommand("SP_Actualizar_Moldura", conexion);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@idMol", objmoldura.PK_IM_Cod);
+            command.Parameters.AddWithValue("@descripcion", objmoldura.VM_Descripcion);
+            command.Parameters.AddWithValue("@imagen", objmoldura.VBM_Imagen);
+            command.Parameters.AddWithValue("@unidadMet", objmoldura.DM_UnidadMetrica);
+            command.Parameters.AddWithValue("@stock",objmoldura.IM_Stock);
+            command.Parameters.AddWithValue("@precio", objmoldura.DM_Precio);
+            command.Parameters.AddWithValue("@estado", objmoldura.IM_Estado);
+            command.Parameters.AddWithValue("@idtipom", objmoldura.FK_ITM_Tipo);
+            conexion.Open();
+            command.ExecuteNonQuery();
+            conexion.Close();
+        }
+            public DataSet desplegabletTipoMoldua()
+        {
+            SqlDataAdapter tipomol = new SqlDataAdapter("SP_Desplegable_Tipo_Moldura", conexion);
+            tipomol.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataSet DS = new DataSet();
+            tipomol.Fill(DS);
+            return DS;
+        }
+
+    }
+}
